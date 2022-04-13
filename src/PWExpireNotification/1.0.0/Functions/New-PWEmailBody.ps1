f6bcdb5c982910b9aae95c0e0939acecacfa3aa6
Daniel Carroll <44060624+dacarroll@users.noreply.github.com>
2022-04-13 08:32:14 -0400

    fix braces of new-pwemailbody

--- src/PWExpireNotification/1.0.0/Functions/New-PWEmailBody.ps1
+++ src/PWExpireNotification/1.0.0/Functions/New-PWEmailBody.ps1
@@ -1,27 +1,29 @@
-﻿[cmdletbinding(SupportsShouldProcess=$true)]
-param (
-  [string]$Subject,
-  [string]$Importance,
-  [String]$Message,
-  [string]$EmailAddress
-)
-if ($PSCmdlet.ShouldProcess(("Subject:{0}; Message: {1}; Recipient(s):{2}" -f $Subject,$Message,$EmailAddress))) {
-    $body = [pscustomobject]@{
-        Message = [pscustomobject]@{
-            Subject = $subject
-            importance = $importance
-            Body = [pscustomobject]@{
-                ContentType = "Text"
-                Content = $Message
-            }
-            ToRecipients = [array][pscustomobject]@{
-                EmailAddress = [pscustomobject]@{
-                    Address = $emailaddress
+﻿function New-PWEmailBody {
+    [cmdletbinding(SupportsShouldProcess=$true)] 
+    param (
+    [string]$Subject,
+    [string]$Importance,
+    [String]$Message,
+    [string]$EmailAddress
+    )
+    if ($PSCmdlet.ShouldProcess(("Subject:{0}; Message: {1}; Recipient(s):{2}" -f $Subject,$Message,$EmailAddress))) {
+        $body = [pscustomobject]@{
+            Message = [pscustomobject]@{
+                Subject = $subject
+                importance = $importance
+                Body = [pscustomobject]@{
+                    ContentType = "Text"
+                    Content = $Message
+                }
+                ToRecipients = [array][pscustomobject]@{
+                    EmailAddress = [pscustomobject]@{
+                        Address = $emailaddress
+                    }
                 }
             }
+            SaveToSentItems = $false
+            isDraft = $false
         }
-        SaveToSentItems = $false
-        isDraft = $false
+        return $body
     }
-    return $body
 }
